e69cb094710068b1da3401e793d67082e32d7aaf
Pablo Chacin <pablochacin@gmail.com>
2025-03-13 14:55:41 +0100

    use new k6 config location
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- cmd/k6config.go
+++ cmd/k6config.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -17,36 +16,42 @@ type k6configFile struct {
 	} `json:"collectors"`
 }
 
-// loadConfig loads the k6 config file from the given path or the default location.
-// if using the default location and the file does not exist, it returns an empty config.
+// loadConfig loads the k6 config file from the given path or the default locations.
+// if using the default locations, if the file does not exist or can't be read, it returns an empty config.
+// default locations are k6/config.json and loadimpact/k6/config.json under the user config directory.
 func loadConfig(configPath string) (k6configFile, error) {
-	var (
-		config       k6configFile
-		usingDefault bool
-		homeDir      string
-		err          error
-	)
-
-	if configPath == "" {
-		usingDefault = true
-		homeDir, err = os.UserConfigDir() //nolint:forbidigo
-		if err != nil {
-			return config, fmt.Errorf("failed to get user home directory: %w", err)
-		}
-		configPath = filepath.Join(homeDir, "loadimpact", "k6", "config.json")
+	if configPath != "" {
+		return readConfig(configPath)
 	}
 
+	var config k6configFile
+	homeDir, err := os.UserConfigDir() //nolint:forbidigo
+	if err != nil {
+		return config, fmt.Errorf("failed to get user config directory: %w", err)
+	}
+	for _, location := range []string{"", "loadimpact"} {
+		configPath = filepath.Join(homeDir, location, "k6", "config.json")
+		config, err = readConfig(configPath)
+		if err == nil {
+			break
+		}
+	}
+
+	// if using default locations we don't return errors if we can't read the file
+	return config, nil
+}
+
+func readConfig(configPath string) (k6configFile, error) {
 	buffer, err := os.ReadFile(configPath) //nolint:forbidigo,gosec
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) && usingDefault { //nolint:forbidigo
-			return config, nil
-		}
-		return config, fmt.Errorf("failed to read config file %q: %w", configPath, err)
+		return k6configFile{}, fmt.Errorf("failed to read config file %q: %w", configPath, err)
 	}
 
+	var config k6configFile
 	err = json.Unmarshal(buffer, &config)
 	if err != nil {
-		return config, fmt.Errorf("failed to parse config file %q: %w", configPath, err)
+		// ensure we return an empty config if the file is not valid
+		return k6configFile{}, fmt.Errorf("failed to parse config file %q: %w", configPath, err)
 	}
 
 	return config, nil
